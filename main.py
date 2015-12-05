bebc473971f6e1325d7ea794480322e9a2f9d861
Karol Dzialowski <karlososhd@gmail.com>
2015-12-05 20:15:14 +0100

    Adding tasks, doing tasks

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from datetime import date, datetime
+import random
+
 __author__ = 'jupiter'
 
 import sqlite3
@@ -19,8 +22,8 @@ def create_db():
     c.execute("""
             CREATE TABLE IF NOT EXISTS `task` (
                 `id_task` INTEGER PRIMARY KEY AUTOINCREMENT,
-                `name` int(11),
-                `short_name` varchar(10),
+                `name` int(11) UNIQUE,
+                `short_name` varchar(10) UNIQUE,
                 `color` int(11),
                 FOREIGN KEY (`color`) REFERENCES `colors` (`id_color`)
             );
@@ -40,29 +43,72 @@ def create_db():
         """
             CREATE TABLE IF NOT EXISTS `colors` (
                 `id_color` INTEGER PRIMARY KEY AUTOINCREMENT,
-                `color` varchar(7)
+                `color` varchar(7) UNIQUE
             );
         """
     )
+    #
+    # c.execute(
+    #     """
+    #         INSERT INTO colors (color) VALUES ("#99E3B6")
+    #     """
+    # )
 
-    c.execute(
-        """
-            INSERT INTO colors (id_color, color) VALUES (1, "#99E3B6")
-        """
-    )
+    conn.commit()
     c.close()
 
-def create_task(task_name):
+def create_task(task_name, short_name=None):
 
     # Connect to DB
     conn = sqlite3.connect(DB_PATH)
     c = conn.cursor()
-
-    values = (task_name, 1)
-    c.execute("INSERT INTO task (name, color) VALUES (?,?)", values)
-
+    c.execute('SELECT id_color FROM colors')
+    result = c.fetchall()
+    id_color = random.choice(result)
+    id_color = id_color[0]
+    values = (task_name, id_color,short_name)
+    try:
+        c.execute("INSERT INTO task (name, color, short_name) VALUES (?,?,?)", values)
+    except ValueError:
+        print "Taki task jest juz w bazie"
+    # http://stackoverflow.com/questions/22488763/sqlite-insert-query-not-working-with-python
+    conn.commit()
     c.close()
 
+def do_task(task_name):
+    conn = sqlite3.connect(DB_PATH)
+    c = conn.cursor()
+
+    # Do this instead
+    t = (task_name,)
+    c.execute('SELECT id_task FROM task WHERE name=?', t)
+    result = c.fetchone()
+    if result:
+        task_id = result[0]
+        print task_id
+    else:
+        c.execute('SELECT id_task FROM task WHERE short_name=?', t)
+        result = c.fetchone()
+        if result:
+            task_id = result[0]
+            print task_id
+        else:
+            c.execute('SELECT id_task FROM task WHERE id_task=?', t)
+            result = c.fetchone()
+            if result:
+                task_id = result[0]
+                print task_id
+            else:
+                print "Brak takiego taska"
+
+    if 'task_id' in locals():
+        today = datetime.now().strftime("%Y-%m-%d")
+        values = (task_id, today)
+        c.execute("INSERT INTO registry (id_task, date) VALUES (?,?)", values)
+
+    conn.commit()
+    c.close()
 
 create_db()
-create_task("programowanie")
+#create_task("dodawanie7", "dodawaj")
+#do_task("dodawanie")
